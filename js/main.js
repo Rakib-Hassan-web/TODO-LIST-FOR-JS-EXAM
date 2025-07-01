fdc8a618cbeaf1fb528feb8e00dcc01f0ff12f00
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-02 01:11:28 +0600

    icon added with hover and value transer

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-
-
 // ------------dom-----------
 
-let pluss =document.querySelector('.pluss')
-let errorr =document.querySelector('.errorr')
-let input1 =document.querySelector('.input1')
-let extra =document.querySelector('.extra')
-let deletbutton =document.querySelector('.deletbutton')
-let editbutton =document.querySelector('.editbutton')
-let donebutton =document.querySelector('.donebutton')
+let pluss = document.querySelector('.pluss')
+let errorr = document.querySelector('.errorr')
+let input1 = document.querySelector('.input1')
+let extra = document.querySelector('.extra')
+let deletbutton = document.querySelector('.deletbutton')
+let editbutton = document.querySelector('.editbutton')
+let donebutton = document.querySelector('.donebutton')
 
 
 // let i=0
@@ -16,58 +14,56 @@ let donebutton =document.querySelector('.donebutton')
 // pluss.innerHTML=i
 // pluss.innerHTML=++i
 
-let handlebutton =()=>{
+let handlebutton = () => {
 
 
-   if(input1.value==''){
-    errorr.innerHTML='faka'
-   }
-
-
-   else {
+   if (input1.value == '') {
+      errorr.innerHTML = 'faka'
+   } else {
 
 
 
       // -------create element-------
 
-      let extraTODO= document.createElement('div')
-      let extrainput= document.createElement('input')
-      let deletbutton= document.createElement('button')
-      let editbutton= document.createElement('button')
-      let donebutton= document.createElement('button')
-    
+      let extraTODO = document.createElement('div')
+      let extrainput = document.createElement('input')
+      let deletbutton = document.createElement('button')
+      let editbutton = document.createElement('button')
+      let donebutton = document.createElement('button')
+
 
 
       // ---------append child-------
 
-     extra.appendChild(extraTODO)
-     extraTODO.appendChild(extrainput)
-     extraTODO.appendChild(deletbutton)
-     extraTODO.appendChild(editbutton)
-     extraTODO.appendChild(donebutton)
+      extra.appendChild(extraTODO)
+      extraTODO.appendChild(extrainput)
+      extraTODO.appendChild(deletbutton)
+      extraTODO.appendChild(editbutton)
+      extraTODO.appendChild(donebutton)
 
-   //   ---------classlist add ----------
+      //   ---------classlist add ----------
 
-   extraTODO.classList.add('extraTODO')
-   extrainput.classList.add('extrainput')
-   deletbutton.classList.add('deletbutton')
-   editbutton.classList.add('editbutton')
-   donebutton.classList.add('donebutton')
-
-
-   //  ---------------icon added---------
+      extraTODO.classList.add('extraTODO')
+      extrainput.classList.add('extrainput')
+      deletbutton.classList.add('deletbutton')
+      editbutton.classList.add('editbutton')
+      donebutton.classList.add('donebutton')
 
 
-   deletbutton.innerHTML='<i class="fa-regular fa-trash-can"></i>'
-
-   editbutton.innerHTML='<i class="fa-regular fa-pen-to-square"></i>'
-
-   donebutton.innerHTML='<i class="fa-regular fa-circle-check"></i>'
+      //  ---------------icon added---------
 
 
+      deletbutton.innerHTML = '<i class="fa-regular fa-trash-can"></i>'
+
+      editbutton.innerHTML = '<i class="fa-regular fa-pen-to-square"></i>'
+
+      donebutton.innerHTML = '<i class="fa-regular fa-circle-check"></i>'
 
 
 
+
+      extrainput.value = input1.value
+      input1.value = ''
 
 
 
